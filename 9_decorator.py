10a13f5ddfce4e4907afbed3d85013dcd34449ad
carlosporta <cmaciasporta@gmail.com>
2021-12-21 08:47:23 -0300

    decorator added

---
+++ 9_decorator.py
@@ -0,0 +1,71 @@
+from abc import ABC, abstractmethod
+
+
+class Notifier(ABC):
+    @abstractmethod
+    def notify(self) -> None:
+        raise NotImplementedError
+
+
+class NotifierDecorator(Notifier):
+    def __init__(self, notifier: Notifier) -> None:
+        self.__notifier = notifier        
+
+    def notify(self) -> None:
+        self.__notifier.notify()
+
+
+class WhatsappNotifier(Notifier):
+    def notify(self) -> None:
+        print(f'Notifying through Whatsapp')
+
+
+class SMSNotifier(Notifier):
+    def notify(self) -> None:
+        print(f'Notifying through SMS')
+
+
+class EmailNotifier(Notifier):
+    def notify(self) -> None:
+        print(f'Notifying through Email')
+
+
+class WhatsappNotifierDecorator(NotifierDecorator):
+    def __init__(self, notifier: Notifier) -> None:
+        super().__init__(notifier)
+        self.__notifier = WhatsappNotifier()
+
+    def notify(self) -> None:
+        self.__notifier.notify()
+        super().notify()
+
+
+class SMSNotifierDecorator(NotifierDecorator):
+    def __init__(self, notifier: Notifier) -> None:
+        super().__init__(notifier)
+        self.__notifier = SMSNotifier()
+
+    def notify(self) -> None:
+        self.__notifier.notify()
+        super().notify()
+
+class EmailNotifierDecorator(NotifierDecorator):
+    def __init__(self, notifier: Notifier) -> None:
+        super().__init__(notifier)
+        self.__notifier = EmailNotifier()
+
+    def notify(self) -> None:
+        self.__notifier.notify()
+        super().notify()
+
+
+if __name__ == "__main__":
+    wn = WhatsappNotifier()
+    en = EmailNotifierDecorator(wn)
+    sn = SMSNotifierDecorator(en)
+
+    wn.notify()
+    print()
+    en.notify()
+    print()
+    sn.notify()
