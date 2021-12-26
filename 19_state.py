fd990040f469cb6e856855063560bf90262c6e03
carlosporta <cmaciasporta@gmail.com>
2021-12-26 17:49:56 -0300

    type fix to state

--- 19_state.py
+++ 19_state.py
@@ -7,33 +7,33 @@ class State(ABC):
         self.status = status
 
     @abstractmethod
-    def publish(self):
+    def publish(self) -> "State":
         raise NotImplementedError
 
 
 class Published(State):
-    def __init__(self, content) -> None:
+    def __init__(self, content: str) -> None:
         super().__init__(content, "published")
 
-    def publish(self):
+    def publish(self) -> "Published":
         return self
 
 
 class Moderation(State):
-    def __init__(self, content) -> None:
+    def __init__(self, content: str) -> None:
         super().__init__(content, "moderation")
 
-    def publish(self):
+    def publish(self) -> Published:
         # simulate a moderation
         self.content = self.content.replace("ERROR", "FIXED")
         return Published(self.content)
     
 
 class Draft(State):
-    def __init__(self, content) -> None:
+    def __init__(self, content: str) -> None:
         super().__init__(content, "draft")
 
-    def publish(self):
+    def publish(self) -> Moderation:
         return Moderation(self.content)
 
 
@@ -41,10 +41,10 @@ class Document:
     def __init__(self, state: State) -> None:
         self.state = state
 
-    def render(self):
+    def render(self) -> None:
         print(self.state.content)
 
-    def publish(self):
+    def publish(self) -> None:
         self.state = self.state.publish()
 
 
